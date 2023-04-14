cd34823fa32116e82ffeb22fe062210ee9e2ac3a
Garadas <art_kob@mail.ru>
2023-04-14 11:38:23 +0000

    Update 7 files

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -16,17 +16,13 @@ const Main=()=>{
         <>
             <div className="header">
                 <div className="row1">
-                    <h1>A room without books is like<br/> a body without a soul.</h1>
-                </div>
-                <div className="row2">
-                    <h2>Find Your Book</h2>
+                    <h1>Book search</h1>
                     <div className="search">
-                        <input type="text" placeholder="Enter Your Book Name"
+                        <input type="text" 
                         value={search} onChange={e=>setSearch(e.target.value)}
                         onKeyPress={searchBook}/>
                         <button><i className="fas fa-search"></i></button>
                     </div>
-                    <img src="./images/bg2.png" alt="" />
                 </div>
             </div>
 
